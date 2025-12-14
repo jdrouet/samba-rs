c34a73183071a12859e654ca8207c1874b1a665a
Jeremie Drouet <jeremie.drouet@gmail.com>
2025-12-14 21:11:25 +0100

    feat: implement TransportCapabilities
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

--- src/entities/v2/negotiate/request.rs
+++ src/entities/v2/negotiate/request.rs
@@ -32,6 +32,8 @@ pub enum ParseError {
     UnknownCapabilities,
     #[error("unknown security modes")]
     UnknownSecurityModes,
+    #[error("unknown transport flags")]
+    UnknownTransportFlags,
 }
 
 bitflags::bitflags! {
@@ -343,6 +345,7 @@ pub enum NegotiateContext<'a> {
     EncryptionCapabilities(EncryptionCapabilities<'a>),
     CompressionCapabilities(CompressionCapabilities<'a>),
     NetNameNegotiateContextId(NetNameNegotiateContextId<'a>),
+    TransportCapabilities(TransportCapabilities),
 }
 
 impl<'a> NegotiateContext<'a> {
@@ -371,6 +374,9 @@ impl<'a> NegotiateContext<'a> {
             NegotiateContextType::NetNameNegotiateContextId => {
                 NetNameNegotiateContextId::parse(buf)
                     .map(NegotiateContext::NetNameNegotiateContextId)
+            }
+            NegotiateContextType::TransportCapabilities => {
+                TransportCapabilities::parse(buf).map(NegotiateContext::TransportCapabilities)
             }
             _ => todo!(),
         }
@@ -649,8 +655,51 @@ impl<'a> NetNameNegotiateContextId<'a> {
     }
 }
 
+bitflags::bitflags! {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct TransportFlags: u32 {
+        /// Transport security is offered to skip SMB2 encryption on this connection.
+        const SMB2_ACCEPT_TRANSPORT_LEVEL_SECURITY = 0x01;
+    }
+}
+
+/// The SMB2_TRANSPORT_CAPABILITIES context is specified in an SMB2 NEGOTIATE request to indicate
+/// transport capabilities over which the connection is made. The format of the data in the Data field of this
+/// SMB2_NEGOTIATE_CONTEXT is as follows.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct TransportCapabilities {
+    pub flags: TransportFlags,
+}
+
+impl TransportCapabilities {
+    pub fn parse(buf: &[u8]) -> Result<Self, ParseError> {
+        let flags = buf.get(0..4).ok_or(ParseError::BufferTooShort)?;
+        let flags = TransportFlags::from_bits(u32_from_le_bytes(flags))
+            .ok_or(ParseError::UnknownTransportFlags)?;
+
+        Ok(Self { flags })
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn should_parse_transport_capabilities() {
+        let cap = super::TransportCapabilities::parse(&[0, 0, 0, 0]).unwrap();
+        assert!(cap.flags.is_empty());
+        let cap = super::TransportCapabilities::parse(&[1, 0, 0, 0]).unwrap();
+        assert!(
+            cap.flags
+                .contains(super::TransportFlags::SMB2_ACCEPT_TRANSPORT_LEVEL_SECURITY)
+        );
+    }
+
+    #[test]
+    fn should_fail_parse_transport_capabilities_too_small() {
+        let err = super::TransportCapabilities::parse(&[0, 0, 0]).unwrap_err();
+        assert_eq!(err, super::ParseError::BufferTooShort);
+    }
+
     #[test]
     fn should_parse_compression_capabilities() {
         let cap = super::CompressionCapabilities::parse(&[0, 0, 0, 0, 0, 0, 0, 0]).unwrap();
